ab6c9807f0c3841589807662e6de143578494344
sree_10 <shreedharansingh@gmail.com>
2026-05-25 23:39:55 +0530

    Time: 6 ms (80.71%) | Memory: 12.4 MB (18.23%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+
+    def isPalindrome(self, x):
+
+        if x < 0:
+            return False
+
+        original = x
+        reverse = 0
+
+        while x != 0:
+            reverse = reverse * 10 + x % 10
+            x //= 10
+
+        return original == reverse
