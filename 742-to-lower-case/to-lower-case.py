86cba5c551c458a00250a88ffc06a29debc5d634
sree_10 <shreedharansingh@gmail.com>
2026-06-05 23:19:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (53.68%) - LeetSync

--- 742-to-lower-case/to-lower-case.py
+++ 742-to-lower-case/to-lower-case.py
@@ -1,11 +1,9 @@
 class Solution:
     def toLowerCase(self, s):
         result = ""
-
         for i in range(len(s)):
             if 'A' <= s[i] <= 'Z':
                 result += chr(ord(s[i]) + 32)
             else:
                 result += s[i]
-
         return result
