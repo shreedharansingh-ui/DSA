19cb7a2c1eb51edef07ab1ebcba9db476ae5d1cb
sree_10 <shreedharansingh@gmail.com>
2026-06-11 22:44:25 +0530

    Time: 3 ms (39.49%) | Memory: 16.7 MB (18.97%) - LeetSync

---
+++ 1899-count-items-matching-a-rule/count-items-matching-a-rule.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countMatches(self, items, ruleKey, ruleValue):
+        if ruleKey == 'type':
+            index = 0
+        elif ruleKey == 'color':
+            index = 1
+        else:
+            index = 2
+        count = 0
+
+        for item in items:
+            if item[index] == ruleValue:
+                count += 1
+        return count
