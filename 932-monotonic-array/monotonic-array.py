9a1c295dfbc0e0b9f890659eb69d6a304d77b196
sree_10 <shreedharansingh@gmail.com>
2026-06-24 21:10:05 +0530

    Time: 59 ms (72.63%) | Memory: 20.3 MB (86.06%) - LeetSync

--- 932-monotonic-array/monotonic-array.py
+++ 932-monotonic-array/monotonic-array.py
@@ -1,7 +1,6 @@
 class Solution:
     def isMonotonic(self, nums):
         inc = dec = True
-
         for i in range(len(nums) - 1):
             if nums[i] < nums[i + 1]:
                 dec = False
